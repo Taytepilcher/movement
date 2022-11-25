0898eeca7e7dc0777f257e666b291b35d7a44e62
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2022-11-25 09:32:58 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,20 @@
+var Xco, Yco,R,speed;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    Xco = width/2;
+    Yco = height/2;
+    R=200;
+    speed= 5;
 }
 
 function draw() {
     background(220);
-
+    Yco= Yco+speed;
+   if(Yco == height+R/2){
+       Yco = -R/2;
+   }
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(Xco,Yco, R);
 }
